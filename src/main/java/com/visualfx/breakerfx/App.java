0cf5915f3a97fde636cea1fdc3e70d898eb5c03e
YovelB <yovel41998@gmail.com>
2022-02-25 21:59:59 +0200

    Initial commit

---
+++ src/main/java/com/visualfx/breakerfx/App.java
@@ -0,0 +1,40 @@
+package com.visualfx.breakerfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class App extends Application {
+    private static final Logger logger = LogManager.getLogger(App.class);
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("MainView"));
+        scene.getStylesheets().add(Objects.requireNonNull(App.class.getResource("styles.css")).toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("views/" + fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        logger.trace("Opening the application.");
+        launch(args);
+        logger.trace("Exiting the application.");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.visualfx.breakerfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    requires org.apache.logging.log4j;
+
+    opens com.visualfx.breakerfx to javafx.fxml;
+    opens com.visualfx.breakerfx.controllers to javafx.fxml;
+    exports com.visualfx.breakerfx;
+}
